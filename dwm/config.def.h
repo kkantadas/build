cec979a2552e69b0cb2c0337f0c0f492f5c79755
kkantadas <krishnas.kanta@gmail.com>
2025-03-26 18:38:23 +0530

    some new

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 3;       /* vertical padding of bar */
 static const int sidepad            = 3;       /* horizontal padding of bar */
